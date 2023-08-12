677ea30a7249b954281b94753b96ae6d9b61e232
Janith3003 <janith.disanayake@cloudparallax.com>
2023-08-12 15:29:56 +0530

    add images to the game Tic Tac Toe

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -1,6 +1,20 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
+const icon_x = new Image();
+icon_x.src = './Images/icon-x.png';
+
+icon_x.onload = function() {
+  ctx.drawImage(icon_x, 15, 12, 100, 110, 25, 25, 150, 150, 0, 0);  
+}
+
+const icon_o = new Image();
+icon_o.src = './Images/icon-o.png';
+
+icon_o.onload = function() {
+  ctx.drawImage(icon_o, 5, 1, 160, 170, 225, 225, 150, 150, 0, 0);  
+}
+
 ctx.strokeStyle = "black"; // Set the line color
 ctx.lineWidth = 2; // Set the line width
 ctx.beginPath();
