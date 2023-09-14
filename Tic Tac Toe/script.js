e1d6215576d3e249c17fe208813e4787e3de41ed
JanithDisanayake <janith.disanayake@cloudparallax.com>
2023-09-14 21:32:09 +0530

    changes Tic Tac Toe

--- Tic Tac Toe/script.js
+++ Tic Tac Toe/script.js
@@ -1,6 +1,11 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
+// const XsAndOs = new Array(3).fill(null).map(() => new Array(3).fill(null));
+// console.log(XsAndOs)
+const XsAndOs = new Array(3).fill(null).map(() => new Array(3).fill(null));
+console.log(XsAndOs)
+
 const icon_x = new Image();
 icon_x.src = './Images/icon-x.png';
 
@@ -15,12 +20,14 @@ icon_o.onload = function() {
   ctx.drawImage(icon_o, 5, 1, 160, 170, 225, 225, 150, 150, 0, 0);  
 }
 
-ctx.strokeStyle = "black"; // Set the line color
-ctx.lineWidth = 2; // Set the line width
+// borders of the game
+
+ctx.strokeStyle = "black";  // Set the line color
+ctx.lineWidth = 2;          // Set the line width
 ctx.beginPath();
-ctx.moveTo(0, 100);     // Starting point (x, y)
-ctx.lineTo(300, 100);   // Ending point (x, y)
-ctx.stroke();           // Stroke the line
+ctx.moveTo(0, 100);         // Starting point (x, y)
+ctx.lineTo(300, 100);       // Ending point (x, y)
+ctx.stroke();               // Stroke the line
 
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
@@ -42,3 +49,41 @@ ctx.beginPath();
 ctx.moveTo(200, 0);
 ctx.lineTo(200, 300);
 ctx.stroke(); 
+
+
+document.addEventListener("mousedown", function (event) {
+  let canvasPosition = canvas.getBoundingClientRect();
+
+  let x = ((event.clientX-canvasPosition.left) - ((event.clientX-canvasPosition.left) % 100)) / 100;
+  let y = ((event.clientY-canvasPosition.top) - ((event.clientY-canvasPosition.top) % 100)) / 100;
+  
+  const icon_x = new Image();
+  icon_x.src = './Images/icon-x.png';
+  
+  icon_x.onload = function() {
+    ctx.drawImage(icon_x, 15, 12, 100, 110, x*100+25, y*100+25, 150, 150, 0, 0);  
+  }
+
+  // XsAndOs[y][x] = icon_x;
+  // XsAndOs.map((e)=>{
+  //   console.log(e)
+  // })
+  
+})
+
+
+
+// document.addEventListener("keyup", function (event) {
+  
+//   if(event.code==="ControlRight" || event.code==="ControlLeft")
+//   {
+//     const icon_o = new Image();
+//     icon_o.src = './Images/icon-o.png';
+    
+//     icon_o.onload = function() {
+//       ctx.drawImage(icon_o, 5, 1, 160, 170, x*100+25, y*100+25, 150, 150, 0, 0); 
+//     }
+
+//   }
+  
+// })
