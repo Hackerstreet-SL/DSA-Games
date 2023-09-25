718d2019e25b0a978570502ce0c17173ffea560a
JanithDisanayake <janith.disanayake@cloudparallax.com>
2023-09-25 23:22:38 +0530

    implement the bestMove to find next possible move Tic Tac Toe MiniMax

--- Tic Tac Toe Minimax/script.js
+++ Tic Tac Toe Minimax/script.js
@@ -2,7 +2,6 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
 admin_mode = false;
-let game_over = false;
 const XsAndOs = new Array(3).fill(null).map(() => new Array(3).fill(null));
 console.log(XsAndOs)
 
@@ -36,31 +35,31 @@ ctx.moveTo(200, 0);
 ctx.lineTo(200, 300);
 ctx.stroke(); 
 
-/**
- * After player make his move
- * This function will executes
- */
-function computersTime () {
-  win_position = logic();
-  console.log(win_position)
-  if ( win_position!=null || win_position != undefined ) {
-    insertImage("O", win_position[0], win_position[1])
-    logic();
-
-  } else if (win_position == undefined ) {
-    unfilled_count = boardIsFull()
-    console.log(unfilled_count.length)
-    if ( unfilled_count.length!=9 && unfilled_count.length!=0 ) {
-      const random = Math.floor(Math.random() * unfilled_count.length)
-      let random_cell = unfilled_count[random][0]
-      insertImage("O", random_cell[1], random_cell[0])
-      logic();
-    }
-    else if ( unfilled_count.length==9 ) {
-      game_over=true;
+function bestMove() {
+  let bestScore = -Infinity;
+  let bestMove;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (XsAndOs[j][i]== null) {
+        XsAndOs[j][i] = 'X'
+        let score = minimax();
+        XsAndOs[j][i] = null
+        if (score > bestScore) {
+          bestScore = score;
+          bestMove = { x:i, y:j };
+        }
+      } 
     }
   }
+  console.log(bestMove)
+  insertImage('X', bestMove.x, bestMove.y)
+  currentPlayer = 'O';
 }
+
+function minimax() {
+  return 1;
+}
+
 
 function boardIsFull () {
   let unfilled_count = []
@@ -100,17 +99,11 @@ function insertImage (imageType, x, y) {
     }
     XsAndOs[y][x] = 'O';
   }
-
-  // XsAndOs[y][x] = icon;
-  // XsAndOs.map((e)=>{
-  //   console.log(e)
-  // })
 }
 
 function drawLine (cells) {
   const from = cells[0]
   const to = cells[2]
-  game_over = true
 
   console.log('from', from, 'to', to)
 
@@ -123,240 +116,21 @@ function drawLine (cells) {
 }
 
 
-function logic () {
-  let win_position = []
-
-  win_position = Verticle_Zero("O")
-  if( win_position != null )
-    return win_position;
-  win_position = Verticle_One("O")
-  if ( win_position != null )
-    return win_position;
-  win_position = Verticle_Two("O")
-  if ( win_position != null )
-    return win_position;
-  win_position = Horizontal_Zero("O")
-  if ( win_position != null )
-    return win_position
-  win_position = Horizontal_One("O")
-  if ( win_position != null )
-    return win_position
-  win_position = Horizontal_Two("O")
-  if ( win_position != null )
-    return win_position
-  win_position = Left_Angle("O")
-  if ( win_position != null )
-    return win_position
-  win_position = Right_Angle("O")
-  if ( win_position != null )
-    return win_position
-
-  win_position = Verticle_Zero("X")
-  if( win_position != null )
-    return win_position;
-  win_position = Verticle_One("X")
-  if ( win_position != null )
-    return win_position;
-  win_position = Verticle_Two("X")
-  if ( win_position != null )
-    return win_position;
-  win_position = Horizontal_Zero ("X")
-  if ( win_position != null )
-    return win_position
-  win_position = Horizontal_One  ("X")
-  if ( win_position != null )
-    return win_position
-  win_position = Horizontal_Two  ("X")
-  if ( win_position != null )
-    return win_position
-  win_position = Left_Angle      ("X")
-  if ( win_position != null )
-    return win_position
-  win_position = Right_Angle     ("X")
-  if ( win_position != null )
-    return win_position
-}
-
-// return array cell
-
-
-function Verticle_Zero (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[i][0]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][0]==null) return [0,i]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][0]!=null) line_cells.push([0,i])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Verticle_One (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[i][1]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][1]==null) return [1,i]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][1]!=null) line_cells.push([1,i])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Verticle_Two (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[i][2]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][2]==null) return [2,i]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][2]!=null) line_cells.push([2,i])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Horizontal_Zero (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[0][i]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[0][i]==null) return [i,0]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[0][i]!=null) line_cells.push([i,0])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Horizontal_One (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[1][i]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[1][i]==null) return [i,1]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[1][i]!=null) line_cells.push([i,1])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Horizontal_Two (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[2][i]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[2][i]==null) return [i,2]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[2][i]!=null) line_cells.push([i,2])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Left_Angle (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[i][i]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][i]==null) return [i,i]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[i][i]!=null) line_cells.push([i,i])
-    }
-    drawLine(line_cells)
-  }
-  return null
-}
-
-function Right_Angle (key) {
-  count = 0;
-  for (let i=0; i<3; i++) {
-    if (XsAndOs[2-i][i]===key) count ++;
-  }
-  if (count==2) {
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[2-i][i]==null) return [i,2-i]
-    }
-
-  } else if (count==3) {
-    const line_cells = []
-    for (let i=0; i<3; i++) {
-      if (XsAndOs[2-i][i]!=null) line_cells.push([i,2-i])
-    }
-    drawLine(line_cells)
-  }
-}
-
-
-
-
-
 document.addEventListener("mousedown", function (event) {
   let canvasPosition = canvas.getBoundingClientRect();
 
   let x = ((event.clientX-canvasPosition.left) - ((event.clientX-canvasPosition.left) % 100)) / 100;
   let y = ((event.clientY-canvasPosition.top) - ((event.clientY-canvasPosition.top) % 100)) / 100;
   
-  if (XsAndOs[y][x]==null && !game_over) {
+  if (XsAndOs[y][x]==null) {
     if (admin_mode) {
-      insertImage("O", x, y)
+      insertImage("X", x, y)
     
     } else {
-      insertImage("X", x, y)
+      insertImage("O", x, y)
     }
-    computersTime();
+    bestMove();
+    console.log(XsAndOs)
   }
 })
 
