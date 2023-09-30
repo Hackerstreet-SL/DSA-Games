96588de2cb78c061cdffd999abc06b95af427db0
Keith <keith@liveroom.dev>
2023-09-30 16:28:24 +0530

    test case for shortest path

--- FindShortestPath/test/script.test.js
+++ FindShortestPath/test/script.test.js
@@ -1,46 +1,39 @@
-import { getRandomLetterPair, shortestPath } from '../public/script.js';
+const { Graph, dijkstra, shortestPath } = require('../public/script.js'); 
 
-describe('shortestPath functions', () => {
-  it('should return a random letter pair from the input string', () => {
-    const input = 'ABCDEFGHIJKL';
+// Create a test graph for the dijkstra and shortestPath functions
+const testGraph = new Graph();
+testGraph.addNode("A");
+testGraph.addNode("B");
+testGraph.addNode("C");
+testGraph.addNode("D");
+testGraph.addEdge("A", "B", 2);
+testGraph.addEdge("A", "C", 1);
+testGraph.addEdge("B", "D", 3);
+testGraph.addEdge("C", "D", 4);
 
-    const [letter1, letter2] = getRandomLetterPair(input);
-
-    expect(letter1).toEqual(expect.any(String));
-    expect(letter2).toEqual(expect.any(String));
-    expect(input.includes(letter1)).toBeTruthy();
-    expect(input.includes(letter2)).toBeTruthy();
+describe('Dijkstra Algorithm', () => {
+  test('Should find the shortest distances', () => {
+    const { distances } = dijkstra(testGraph, "A");
+    expect(distances.get("A")).toBe(0);
+    expect(distances.get("B")).toBe(2);
+    expect(distances.get("C")).toBe(1);
+    expect(distances.get("D")).toBe(3);
   });
 
-  it('should throw an error if the input string is less than 2 letters', () => {
-    const input = 'A';
+  test('Should handle unconnected nodes', () => {
+    const { distances } = dijkstra(testGraph, "A");
+    expect(distances.get("E")).toBe(undefined); // "E" is not connected to the graph
+  });
+});
 
-    expect(() => getRandomLetterPair(input)).toThrowError();
+describe('Shortest Path', () => {
+  test('Should find the shortest path', () => {
+    const path = shortestPath(testGraph, "A", "D");
+    expect(path).toEqual(["A", "B", "D"]);
   });
 
-  it('should return the shortest path between two nodes in a valid graph', () => {
-    const graph = new Graph();
-    graph.addNode('A');
-    graph.addNode('B');
-    graph.addNode('C');
-    graph.addEdge('A', 'B', 10);
-    graph.addEdge('B', 'C', 5);
-
-    const shortestPathResult = shortestPath(graph, 'A', 'C');
-
-    expect(shortestPathResult).toEqual(['A', 'B', 'C']);
-  });
-
-  it('should throw an error if the graph is invalid', () => {
-    const graph = new Graph();
-
-    expect(() => shortestPath(graph, 'A', 'B')).toThrowError();
-  });
-
-  it('should throw an error if a node is missing from the graph', () => {
-    const graph = new Graph();
-    graph.addNode('A');
-
-    expect(() => shortestPath(graph, 'A', 'B')).toThrowError();
+  test('Should handle unconnected nodes', () => {
+    const path = shortestPath(testGraph, "A", "E");
+    expect(path).toEqual([]); // No path to "E"
   });
 });
