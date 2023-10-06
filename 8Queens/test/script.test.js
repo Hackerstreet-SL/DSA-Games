dec122a993e70c519debd2163953d684f200255e
JanithDisanayake <janith.disanayake@cloudparallax.com>
2023-10-07 00:13:14 +0530

    solve dom issue in test-case 8Queens

--- 8Queens/test/script.test.js
+++ 8Queens/test/script.test.js
@@ -1,60 +1,84 @@
 // Import the necessary functions from the script.js file
-const { solutions, isSafe, isDiagonalSafe, isStraightSafe } = require('../public/script.js');
+const {
+  solutions,
+  isSafe,
+  isDiagonalSafe,
+  isStraightSafe,
+} = require("../public/script.js");
+const { JSDOM } = require("jsdom");
+
+const chessboardSize = 8;
+let board;
+
+const dom = new JSDOM("<!doctype html><html><body></body></html>", {
+  url: "http://localhost",
+});
+
+global.document = dom.window.document;
+global.window = dom.window;
+global.navigator = dom.window.navigator;
+
+beforeEach(() => {
+  // Reset Chess Board before each test
+  board = Array.from({ length: chessboardSize }, () =>
+    Array(chessboardSize).fill(0)
+  );
+});
 
 // Test suite for Eight Queens Game Script
-describe('Eight Queens Game Script', () => {
-    // Describe the 'solutions' function tests
-    describe('solutions function', () => {
-        // Test case: it should find all possible solutions
-        it('should find all possible solutions', () => {
-            // Call the solutions function with an initial row of 0
-            const result = solutions(0);                                                // Assuming solutions function is correctly implemented
-            expect(result).toHaveLength(expectedSolutionCount);
-        });
+describe("Eight Queens Game Script", () => {
+  // Describe the 'solutions' function tests
+  describe("solutions function", () => {
+    // Test case: it should find all possible solutions
+    it("should find all possible solutions", () => {
+      // Call the solutions function with an initial row of 0
+      const result = solutions(0); // Assuming solutions function is correctly implemented
+      expect(result).toHaveLength(expectedSolutionCount);
     });
+  });
 
-    // Describe the 'isSafe' function tests
-    describe('isSafe function', () => {
-        // Test case: it should return true for safe queen placement
-        it('should return true for safe queen placement', () => {
-            const safe = isSafe(2, 3);                                                  // Assuming (2, 3) is a safe position
-            // Expect the 'safe' variable to be true
-            expect(safe).toBe(true);
-        });
-        // Test case: it should return false for unsafe queen placement
-        it('should return false for unsafe queen placement', () => {
-            const unsafe = isSafe(3, 4);                                                // Assuming (3, 4) is an unsafe position
-            // Expect the 'unsafe' variable to be false
-            expect(unsafe).toBe(false);
-        });
+  // Describe the 'isSafe' function tests
+  describe("isSafe function", () => {
+    // Test case: it should return true for safe queen placement
+    it("should return true for safe queen placement", () => {
+      const safe = isSafe(2, 3); // Assuming (2, 3) is a safe position
+      // Expect the 'safe' variable to be true
+      expect(safe).toBe(true);
     });
+    // Test case: it should return false for unsafe queen placement
+    it("should return false for unsafe queen placement", () => {
+      const unsafe = isSafe(3, 4); // Assuming (3, 4) is an unsafe position
+      // Expect the 'unsafe' variable to be false
+      expect(unsafe).toBe(false);
+    });
+  });
 
-    // Describe the 'isDiagonalSafe' function tests
-    describe('isDiagonalSafe function', () => {
-        // Test case: it should return true for safe diagonal placement
-        it('should return true for safe diagonal placement', () => {
-            const safe = isDiagonalSafe(2, 3);                                          // Assuming (2, 3) is a safe diagonal position
-            // Expect the 'safe' variable to be true
-            expect(safe).toBe(true);
-        });
-        // Test case: it should return false for unsafe diagonal placement
-        it('should return false for unsafe diagonal placement', () => {
-            const unsafe = isDiagonalSafe(3, 4);                                        // Assuming (3, 4) is an unsafe diagonal position
-            expect(unsafe).toBe(false);
-        });
+  // Describe the 'isDiagonalSafe' function tests
+  describe("isDiagonalSafe function", () => {
+    // Test case: it should return true for safe diagonal placement
+    it("should return true for safe diagonal placement", () => {
+      const safe = isDiagonalSafe(2, 3); // Assuming (2, 3) is a safe diagonal position
+      // Expect the 'safe' variable to be true
+      expect(safe).toBe(true);
     });
+    // Test case: it should return false for unsafe diagonal placement
+    it("should return false for unsafe diagonal placement", () => {
+      const unsafe = isDiagonalSafe(3, 4); // Assuming (3, 4) is an unsafe diagonal position
+      expect(unsafe).toBe(false);
+    });
+  });
 
-    // Describe the 'isStraightSafe' function tests
-    describe('isStraightSafe function', () => {
-        // Test case: it should return true for safe straight placement
-        it('should return true for safe straight placement', () => {
-            const safe = isStraightSafe(2, 3);                                          // Assuming (2, 3) is a safe straight position
-            expect(safe).toBe(true);
-        });
-        // Test case: it should return false for unsafe straight placement
-        it('should return false for unsafe straight placement', () => {
-            const unsafe = isStraightSafe(3, 4);                                       // Assuming (3, 4) is an unsafe straight position
-            expect(unsafe).toBe(false);
-        });
+  // Describe the 'isStraightSafe' function tests
+  describe("isStraightSafe function", () => {
+    // Test case: it should return true for safe straight placement
+    it("should return true for safe straight placement", () => {
+      const safe = isStraightSafe(2, 3); // Assuming (2, 3) is a safe straight position
+      expect(safe).toBe(true);
     });
+    // Test case: it should return false for unsafe straight placement
+    it("should return false for unsafe straight placement", () => {
+      const unsafe = isStraightSafe(3, 4); // Assuming (3, 4) is an unsafe straight position
+      expect(unsafe).toBe(false);
+    });
+  });
 });
